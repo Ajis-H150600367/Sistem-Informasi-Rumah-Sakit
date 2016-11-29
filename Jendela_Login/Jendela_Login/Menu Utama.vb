8a7c96d705722d77e116c3c316ebfa204acc351f
AGIL_KILLER\pc <ajispoltanesa.mi2015@gmail.com>
2016-11-29 21:20:17 +0800

    Update data source new project

--- Jendela_Login/Jendela_Login/Menu Utama.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Menu_Utama
-
-    Private Sub PasienToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PasienToolStripMenuItem.Click
-        Data_Pasien.ShowDialog()
-    End Sub
-End Class
