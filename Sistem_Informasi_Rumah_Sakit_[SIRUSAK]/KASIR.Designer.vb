816741a3fc23c673e338d8642816a5473dbb5e54
AGIL_KILLER\pc <ajispoltanesa.mi2015@gmail.com>
2017-01-03 10:13:42 +0800

    update 2017

--- Sistem_Informasi_Rumah_Sakit_[SIRUSAK]/KASIR.Designer.vb
+++ Sistem_Informasi_Rumah_Sakit_[SIRUSAK]/KASIR.Designer.vb
@@ -28,7 +28,7 @@ Partial Class KASIR
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(809, 329)
+        Me.ClientSize = New System.Drawing.Size(827, 344)
         Me.Name = "KASIR"
         Me.Text = "KASIR"
         Me.ResumeLayout(False)
